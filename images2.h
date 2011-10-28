944031d5cc69f63ad3709472475ccbfa56af2680
Martin Holicky <martin.holicky@gmail.com>
2011-10-28 20:33:52 +0200

    Stable version, with som files added

---
+++ images2.h
@@ -0,0 +1,21 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.2, Unix */
+/* Datafile: /home/matoha/Stiahnuté/exam-challenge/images2.dat */
+/* Date: Fri Oct 28 19:15:51 2011 */
+/* Do not hand edit! */
+
+#define img2_credits                     0        /* BMP  */
+#define img2_loading                     1        /* BMP  */
+#define img2_loading2                    2        /* BMP  */
+#define img2_logo                        3        /* BMP  */
+#define img2_random1                     4        /* BMP  */
+#define img2_random10                    5        /* BMP  */
+#define img2_random2                     6        /* BMP  */
+#define img2_random3                     7        /* BMP  */
+#define img2_random4                     8        /* BMP  */
+#define img2_random5                     9        /* BMP  */
+#define img2_random6                     10       /* BMP  */
+#define img2_random7                     11       /* BMP  */
+#define img2_random8                     12       /* BMP  */
+#define img2_random9                     13       /* BMP  */
+#define img2_COUNT                       14
+
